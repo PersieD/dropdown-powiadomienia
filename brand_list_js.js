0d171860b408caaa673e2962de8f1af76b22ec90
PersieD <mdolebska@st.swps.edu.pl>
2025-06-10 14:25:16 +0200

    powiadomienia 1

---
+++ brand_list_js.js
@@ -0,0 +1,6 @@
+const brandList = [
+  "A.BOCCA", "A.S.98", "ACBC", "adidas", "adidas by Stella McCartney", "adidas Golf", "adidas Neo", "adidas Originals", "adidas Performance", "adidas Sportswear",
+  "Adidas Terrex", "Adieu", "ADOLFO DOMINGUEZ", "Ahluwalia", "Aigle", "AIGNER", "aim’n®", "Albano", "ALDO", "Alexandre Birman",
+  // ... (pełna lista marek - pominięta tutaj dla czytelności)
+  "Wojas", "Womsh", "Wonders", "Woolrich", "Wrangler", "Xti", "Zaxy", "Ziener", "Zign", "Zinda", "Zouri", "Zyga"
+];
